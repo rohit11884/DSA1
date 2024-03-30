854e0613d610a65f4bff6efc19d7e3dc2f230218
Rohit <rohit11884kr@gmail.com>
2024-03-30 12:14:23 +0530

    zdf

--- hashing_num.java
+++ hashing_num.java
@@ -1,26 +1,27 @@
 import java.util.*;
 class hashing_num {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the size of array");
-        int n=sc.nextInt();
-        int arr[]=new int[n];
-        System.out.println("Enter the elements in array");
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
-        
-        int hash[]=new int[100000000];
-        for(int i=0;i<n;i++){
-            hash[arr[i]]+=1;
-        }
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the size of array");
+            int n=sc.nextInt();
+            int arr[]=new int[n];
+            System.out.println("Enter the elements in array");
+            for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+            }
+            
+            int hash[]=new int[100000000];
+            for(int i=0;i<n;i++){
+                hash[arr[i]]+=1;
+            }
 
-        System.out.println("Enter the number of element You want to find its repetation");
-        int q=sc.nextInt();
-        System.out.println("Enter the number");
-        while(q--!=0){
-            int num=sc.nextInt();
-            System.out.println(num+" is repeating "+hash[num]+" times");
+            System.out.println("Enter the number of element You want to find its repetation");
+            int q=sc.nextInt();
+            System.out.println("Enter the number");
+            while(q--!=0){
+                int num=sc.nextInt();
+                System.out.println(num+" is repeating "+hash[num]+" times");
+            }
         }
     }
 }
--- hasing_string.java
+++ hasing_string.java
@@ -1,25 +1,26 @@
 import java.util.*;
 public class hasing_string {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String s="fsdfsdfdfSSAADaaa";
+        try (Scanner sc = new Scanner(System.in)) {
+            String s="fsdfsdfdfSSAADaaa";
 
-        // int hash[]=new int[26];
-        int hash[]=new int[256]; // For both upper and lower case
-        for(int i=0;i<s.length();i++){
-            // hash[s.charAt(i-'a')]++;  // for lower case
-            // hash[s.charAt(i-'A')]++;  // for upper case
-            hash[s.charAt(i)]++;  // for both
-        }
-        System.out.println("Enter the number of characters you want to find its repetation ");
-        int q=sc.nextInt();
-        System.out.println("Enter the character");
-        while (q--!=0) {
-            char c=sc.next().charAt(0);
+            // int hash[]=new int[26];
+            int hash[]=new int[256]; // For both upper and lower case
+            for(int i=0;i<s.length();i++){
+                // hash[s.charAt(i-'a')]++;  // for lower case
+                // hash[s.charAt(i-'A')]++;  // for upper case
+                hash[s.charAt(i)]++;  // for both
+            }
+            System.out.println("Enter the number of characters you want to find its repetation ");
+            int q=sc.nextInt();
+            System.out.println("Enter the character");
+            while (q--!=0) {
+                char c=sc.next().charAt(0);
 
-            // System.out.println(c+" is repeating "+hash[c-'a']+" times"); // for lower case
-            // System.out.println(c+" is repeating "+hash[c-'A']+" times"); // for upper case
-            System.out.println(c+" is repeating "+hash[c]+" times");    // for both
+                // System.out.println(c+" is repeating "+hash[c-'a']+" times"); // for lower case
+                // System.out.println(c+" is repeating "+hash[c-'A']+" times"); // for upper case
+                System.out.println(c+" is repeating "+hash[c]+" times");    // for both
+            }
         }
     }
 }
